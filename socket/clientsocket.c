12a2aa648633cd26e2a45d97cbf86ad60ac06cef
smallbomb <rockon590+git@gmail.com>
2017-07-06 10:03:49 +0800

    [BUGFIX] default ip setting error
    client  ip change from 172.0.0.1 to 127.0.0.1

--- socket/clientsocket.c
+++ socket/clientsocket.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]){
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));  
     serv_addr.sin_family = AF_INET;  
-    serv_addr.sin_addr.s_addr = inet_addr("172.0.0.1");  
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  
     serv_addr.sin_port = htons(5566); 
     connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
    
